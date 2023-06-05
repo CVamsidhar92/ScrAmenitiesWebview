ace9ee9ab923cc1e3107aaca7ba815a7901ffbe7
CVamsidhar92 <cvamsidhar92@gmail.com>
2023-06-05 15:44:35 +0530

    Updated on 05.06.23

--- Screens/MapsWebview.js
+++ Screens/MapsWebview.js
@@ -5,7 +5,8 @@ import {useRoute} from '@react-navigation/native';
 
 export default function MapsWebview() {
    const route = useRoute();
-  const geturl = route.params;
+  const {geturl} = route.params;
+  console.log(geturl)
   return (
     <View style={styles.container}>
       <WebView
